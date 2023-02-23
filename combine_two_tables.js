c01ebb0c21cde30b6f5212ee007035899599bdf1
nabilhayet <nabil4457@yahoo.com>
2023-02-22 21:34:39 -0500

    added two tables

---
+++ combine_two_tables.js
@@ -0,0 +1,10 @@
+// Write your MySQL query statement below
+
+/* 
+
+select Person.lastName, Person.firstName, Address.city, Address.state
+from Person 
+left join Address 
+on Person.personId = Address.personId
+
+*/
