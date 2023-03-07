b2fd792f9ac9867078bef70903158b588bd4ce70
nabilhayet <nabil4457@yahoo.com>
2023-03-07 11:11:14 -0500

    added power of three

---
+++ power_of_three.js
@@ -0,0 +1,12 @@
+var isPowerOfThree = function (n) {
+
+    if (n <= 0) return false
+
+    while (n != 0) {
+        if (n == 1) return true
+        if (n % 3 != 0) return false
+        n = n / 3
+    }
+
+    return true
+};
