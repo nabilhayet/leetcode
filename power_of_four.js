6521e43b4f420a9daf159d06a2ed871676169567
nabilhayet <nabil4457@yahoo.com>
2023-03-08 11:32:15 -0500

    added power of four

---
+++ power_of_four.js
@@ -0,0 +1,11 @@
+var isPowerOfFour = function (n) {
+
+    if (n <= 0) return false
+
+    while (n != 0) {
+        if (n == 1) return true
+
+        if (n % 4 != 0) return false
+        n = n / 4
+    }
+};
