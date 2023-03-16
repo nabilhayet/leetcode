fd01e2581679f353199cb0c727ace887646e0005
nabilhayet <nabil4457@yahoo.com>
2023-03-16 11:12:45 -0400

    added two arrays intersection part 2

---
+++ intersection_of_two_Arrays2.js
@@ -0,0 +1,15 @@
+var intersect = function (nums1, nums2) {
+
+    let result = []
+
+    for (let i = 0; i < nums1.length; i++) {
+        if (nums2.includes(nums1[i])) {
+            let index = nums2.indexOf(nums1[i]);
+
+            result.push(nums1[i])
+            nums2.splice(index, 1)
+        }
+    }
+
+    return result
+};
