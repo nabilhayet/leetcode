b75ae0e9ed46cbc89757de80f903f7f7d1fed228
nabilhayet <nabil4457@yahoo.com>
2023-04-03 14:24:14 -0400

    added substring updated

--- repeated_substring_pattern.js
+++ repeated_substring_pattern.js
@@ -2,27 +2,34 @@ var repeatedSubstringPattern = function (s) {
 
     if (s.length == 1) return false
 
-    let str = new Map()
-    let value = 0
+    let arr = []
+    let index = 0
 
     for (let i = 0; i < s.length; i++) {
 
-        if (str[s[i]]) {
-            str[s[i]] += 1
+        if (!arr.includes(s[i])) {
+            arr.push(s[i])
         } else {
-            str[s[i]] = 1
-        }
-    }
-
-    for (const s in str) {
-
-        if (value == 0) {
-            value = str[s]
-        } else {
-            if (value != str[s]) {
+            if (s[i] == arr[index]) {
+                index += 1
+            } else {
                 return false
             }
         }
+
+        if (index == arr.length) {
+            index = 0
+        }
+
+
     }
-    return true
+
+    if (index == 0) {
+        return true
+    } else {
+        return false
+    }
+
+
+    // console.log(arr)
 };
