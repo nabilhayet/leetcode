50e24841b9e099dd512ac61bed396df66d64274f
nabilhayet <nabil4457@yahoo.com>
2023-04-11 14:45:33 -0400

    added maximum consecutive one

---
+++ maximum_consecutive_one.js
@@ -0,0 +1,20 @@
+var findMaxConsecutiveOnes = function (nums) {
+
+    let count = 0
+    let max = 0
+
+    for (let i = 0; i < nums.length; i++) {
+
+        if (nums[i] == 1) {
+            count += 1
+        } else {
+            max = Math.max(count, max)
+            count = 0
+        }
+    }
+
+    max = Math.max(count, max)
+
+    return max
+
+};
