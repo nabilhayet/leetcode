d331a6ca9e0f8836bf81f33933c3872073f2894c
nabilhayet <nabil4457@yahoo.com>
2023-04-20 10:34:44 -0400

    added symmetric tree

---
+++ symmetric_tree.js
@@ -0,0 +1,15 @@
+var isSymmetric = function (root) {
+
+    if (root == null) return true;
+    return isMirror(root.left, root.right);
+};
+
+const isMirror = (leftNode, rightNode) => {
+    if (leftNode == null && rightNode == null) return true;
+    if (leftNode == null || rightNode == null) return false;
+    if (leftNode.val !== rightNode.val) return false;
+
+    return isMirror(leftNode.left, rightNode.right) && isMirror(leftNode.right, rightNode.left);
+
+
+};
