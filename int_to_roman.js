71e24bc9fbe24b4185c7f2559635c06f358a7e79
nabilhayet <nabil4457@yahoo.com>
2023-05-25 12:37:00 -0400

    added int to roman

--- int_to_roman.js
+++ int_to_roman.js
@@ -1,40 +1,18 @@
 var intToRoman = function (num) {
 
-    let result = "";
-    let n;
+    let roman = {
+        'M': 1000, 'CM': 900, 'D': 500, 'CD': 400,
+        'C': 100, 'XC': 90, 'L': 50, 'XL': 40,
+        'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1
+    },
+        romanNum = '';
 
-    let itr = new Map()
-
-    itr.set(1, "I")
-    itr.set(5, "V")
-    itr.set(10, "X")
-    itr.set(50, "L")
-    itr.set(100, "C")
-    itr.set(500, "D")
-    itr.set(1000, "M")
-
-    while (num > 0) {
-
-        if (num >= 1000) {
-
-            // result = result + itr.get("1000")
-            // num = num % 1000
-
-        } else if (num >= 900 && num < 1000) {
-
-        } else if (num >= 400 && num < 900) {
-
-        } else if (num >= 90 && num < 100) {
-
-        } else if (num >= 10 && num < 50) {
-
-        } else if (num >= 5 && num < 10) {
-
-        } else if (num >= 1 && num < 5) {
-
+    for (let i in roman) {
+        while (num >= roman[i]) {
+            romanNum += i;
+            num -= roman[i];
         }
     }
-
-    return result;
+    return romanNum;
 
 };
